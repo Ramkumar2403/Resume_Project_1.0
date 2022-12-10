cb8a15fabbfbe8e8b3874608fcbf80c468d06d39
Ramkumar Megavarnan <67353220+ramkumar-coder2403@users.noreply.github.com>
2022-12-10 12:54:25 +0530

    Update index-js.js

--- assets/custom/index-js.js
+++ assets/custom/index-js.js
@@ -79,7 +79,7 @@ $(function () {
   // Set a same-site cookie for first-party contexts
   document.cookie = 'cookie1=value1; SameSite=Lax';
   // Set a cross-site cookie for third-party contexts
-  document.cookie = 'cookie2=value2; SameSite=None; NotSecure;';
+  document.cookie = 'cookie2=value2; SameSite=None; Secure;';
   
   var element = document.getElementById("top");
 
