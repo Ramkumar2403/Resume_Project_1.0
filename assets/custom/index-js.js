9525d0b7f7c8df5e3c1b0013d17bcbc961edaac9
Ramkumar Megavarnan <67353220+ramkumar-coder2403@users.noreply.github.com>
2022-12-10 14:16:35 +0530

    Update index-js.js
    Remove cookies

--- assets/custom/index-js.js
+++ assets/custom/index-js.js
@@ -77,9 +77,9 @@ window.onload = function () {
 //SCROLL_EVENT FLOAT_BUUTON
 $(function () {
   // Set a same-site cookie for first-party contexts
-  document.cookie = 'cookie1=value1; SameSite=Lax';
+  //document.cookie = 'cookie1=value1; SameSite=Lax';
   // Set a cross-site cookie for third-party contexts
-  document.cookie = 'cookie2=value2; SameSite=None; Secure;';
+  //document.cookie = 'cookie2=value2; SameSite=None; Secure;';
   
   var element = document.getElementById("top");
 
