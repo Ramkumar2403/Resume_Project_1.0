a304d52f6241f30fc445f3b94730a177648ed1a6
Ramkumar Megavarnan <67353220+ramkumar-coder2403@users.noreply.github.com>
2022-12-10 12:52:19 +0530

    Update index-js.js
    add cookies

--- assets/custom/index-js.js
+++ assets/custom/index-js.js
@@ -76,6 +76,11 @@ window.onload = function () {
 //TEXT_REPEAT END
 //SCROLL_EVENT FLOAT_BUUTON
 $(function () {
+  // Set a same-site cookie for first-party contexts
+  document.cookie = 'cookie1=value1; SameSite=Lax';
+  // Set a cross-site cookie for third-party contexts
+  document.cookie = 'cookie2=value2; SameSite=None; NotSecure;';
+  
   var element = document.getElementById("top");
 
   document.getElementById("home").classList.add("active");
